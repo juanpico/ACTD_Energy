648b60fe7cb317392bdcd3a24e3da2694959ff2e
Juan Camilo Pico <juanca.pico@hotmail.com>
2022-12-08 16:35:04 -0500

    Dashboard v1: hace el pronóstico con el .pkl

---
+++ Dashboard/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c321ecef851bed9d7e53f1de97046bf7498cff22af57fdc25e899798eb6fc3b
+size 10224
---
+++ Dashboard/assets/resizing.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6dab0f5818c7d1d018ab91a96283e5c423abf6b048e229874387da8c962fd78d
+size 292
