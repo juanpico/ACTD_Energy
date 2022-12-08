86a4858ef60df93010b515018e87e05e49ff4370
Juan Camilo Pico <juanca.pico@hotmail.com>
2022-12-08 17:22:15 -0500

    Dashboard: sin archivos de datos

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33520119fa779311c36bda688c5ec196a61c6f5e08fcdca12326f023cdde034f
-size 8548
+oid sha256:c82cc6154794e47aae69ced044fb4b9ecd3ef56af685ef306e833866ce8c2ecb
+size 8547
