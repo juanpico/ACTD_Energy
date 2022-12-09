d9cc25149ee4076cb333abdd4768fac5d69b9af1
Mauricio Ricardo Delgado <67530745+mauricio1308@users.noreply.github.com>
2022-12-08 22:12:41 -0500

    Actualizar Dashboard

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c82cc6154794e47aae69ced044fb4b9ecd3ef56af685ef306e833866ce8c2ecb
-size 8547
+oid sha256:7c75a2e4af0514064c5e609320cbe74673770c11b6194c27ef4b1f5d8e3214a5
+size 8546
